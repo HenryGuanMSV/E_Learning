6d2c2c6abaec1223ba8538c59e21b455319bbf3f
sin <haowen.guan@icloud.com>
2022-08-02 11:29:48 +0800

    --- 构建项目(sin)

---
+++ E_Learning/Supporting Files/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  E_Learning
+//
+//  Created by sin kwan on 2022/8/2.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let vc = ViewController()
+        window?.backgroundColor = .white
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ E_Learning/Supporting Files/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  E_Learning
+//
+//  Created by sin kwan on 2022/8/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
